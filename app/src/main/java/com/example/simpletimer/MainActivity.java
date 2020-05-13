0e68f9d9a7ea57e31d9c46e30eb0ed72ee1b5119
Parth Sharma <parthsharma151@gmail.com>
2020-05-13 09:40:15 +0530

    added support for textview to now display time

--- app/src/main/java/com/example/simpletimer/MainActivity.java
+++ app/src/main/java/com/example/simpletimer/MainActivity.java
@@ -3,15 +3,53 @@ package com.example.simpletimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-    boolean timerState = true;
+    boolean timerState = false;
+
+    public void setTimer(int i)
+    {
+        TextView timerValue = (TextView) findViewById(R.id.textView);
+        int minutes = i/6000;
+        int seconds = (i%6000)/100;
+
+        timerValue.setText(minutes+":"+seconds);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar timeSeekBar = (SeekBar) findViewById(R.id.seekBar);
+        timeSeekBar.setMax(20);
+
+        timeSeekBar.setProgress(10);
+        setTimer(10);
+
+        timeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b)
+            {
+                setTimer(i*3000);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+        });
     }
 }
