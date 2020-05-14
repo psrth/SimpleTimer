d6309566e87f8c1148d7b818ee4269e2005bc6c4
Parth Sharma <parthsharma151@gmail.com>
2020-05-14 21:54:03 +0530

    added comments

--- app/src/main/java/com/example/simpletimer/MainActivity.java
+++ app/src/main/java/com/example/simpletimer/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.simpletimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -14,19 +13,28 @@ import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    // variables to keep track of time left at different points
     int time = 0;
+    int millisec;
+
+    // variables to keep track of the states of the timer
+    boolean isActive = false;
+    boolean pause;
+
+    // creating new objects for the different elements
     MediaPlayer mediaPlayer;
     TextView timerTextView;
-    boolean isActive = false;
     CountDownTimer countDownTimer;
     SeekBar timerSeekBar;
     Button restartButton;
     Button pauseButton;
-    boolean pause;
-    int millisec;
 
 
+    // method run when go/restart button is clicked
     public void onClick (View view) {
+
+        // if the timer is not running
         if (!isActive) {
             isActive = true;
             restartButton.setText("RESET");
@@ -37,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
             final int millisInFuture = time * 1000;
             Log.i("Info", Integer.toString(millisInFuture));
 
+            // countdown started
             countDownTimer = new CountDownTimer(millisInFuture, 1000) {
                 public void onTick(long millisecondsUntilDone) {
                     int milli = (int) millisecondsUntilDone / 1000;
@@ -55,17 +64,21 @@ public class MainActivity extends AppCompatActivity {
                         secondsString = Integer.toString(seconds);
                     }
 
+                    // text view that displays the time left
                     timerTextView.setText(Integer.toString(minutes) + ":" + secondsString);
 
+                    // stores the last countdown value in case user pauses
                     millisec = (int) millisecondsUntilDone;
                 }
 
+                // once countdown finishes, the alarm is played
                 @Override
                 public void onFinish() {
                     mediaPlayer.start();
                 }
             }.start();
 
+        // in case the timer is active or the timer is paused — and then reset
         } else if (isActive || pause) {
             isActive = false;
             timerTextView.setText("0:30");
@@ -77,14 +90,20 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // method run when the user clicks the pause/play button
     public void onClickPause (View view) {
+
+        // if the timer was currently not paused
         if (!pause) {
             pause = true;
             countDownTimer.cancel();
             pauseButton.setText("RESUME");
+
+        // if the timer was paused
         } else {
             pause = false;
 
+            // a new countdown is created with the last recorded value
             countDownTimer = new CountDownTimer(millisec, 1000) {
                 public void onTick(long millisecondsUntilDone) {
                     int milli = (int) millisecondsUntilDone / 1000;
@@ -117,6 +136,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    // on create method initialises all the objects for the various elements used
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -128,10 +148,12 @@ public class MainActivity extends AppCompatActivity {
         timerSeekBar = (SeekBar) findViewById(R.id.seekBar);
         timerTextView = (TextView) findViewById(R.id.textView);
 
+        // the default value of the seekbar is one minute, max value is 5 minutes
         timerSeekBar.setMax(300);
         timerSeekBar.setProgress(60);
         time = 60;
 
+        // listener for whenever the timer seek bar is changed
         timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
@@ -149,6 +171,7 @@ public class MainActivity extends AppCompatActivity {
                     secondsString = Integer.toString(seconds);
                 }
 
+                // text changes responding to the user's scrubbing
                 timerTextView.setText(Integer.toString(minutes)+":"+secondsString);
             }
 
@@ -161,3 +184,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
+
+// thanks for reading till here, i appreciate it
+// a: parth sharma, v: 1.5, d: 14-05-20 ————— SIMPLE TIMER APP
